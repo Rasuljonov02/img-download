e0ccea795c71e5e2ce10f944bd3e72ddcf8b6fac
muhammad <muhammadjonrasuljonov4202@gmail.com>
2024-06-24 16:52:38 +0500

    fix: img size

--- instagram dovnload/src/api/index.ts
+++ instagram dovnload/src/api/index.ts
@@ -3,7 +3,7 @@ const getImages = async (link: string ) => {
   const config = {
     method: 'get',
 
-    url: `https://api.pexels.com/v1/search/?page=1&per_page=100&query=${link}`,
+    url: `https://api.pexels.com/v1/search/?page=1&per_page=50&query=${link}`,
     headers: {
       'Authorization': 'Z0lnTOuDR7xtjtxV5kaF3hs5me6HnJJz0rCwtKdCAKeY1Btx6ErBeMHk',
       'Cookie': '__cf_bm=e3.axVB6jyWpBzUKafcfX4nXO0VDq03GhE2Y0hCs2Ks-1719210327-1.0.1.1-jMH7TDWsqJvJWiQE7B_BF56g3FM4fgihnozIRQe76NcdmG_9zKBqjyVdX59FHUigKzzp9SVJSBRXGKMlZZA7tw'
