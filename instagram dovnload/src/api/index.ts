12921be111bf4a78bb18b1e587f71955dba82d39
muhammad <muhammadjonrasuljonov4202@gmail.com>
2024-06-24 16:15:31 +0500

    fix: img size

--- instagram dovnload/src/api/index.ts
+++ instagram dovnload/src/api/index.ts
@@ -32,7 +32,6 @@ const getImages = async (link:string) => {
 
   try {
     const response = await axios(config);
-    console.log(JSON.stringify(response.data));
     return response.data;
   } catch (error) {
     console.error(error);
